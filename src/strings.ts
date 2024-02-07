b1fa59379b1ec20422f1082a15315bcf015db7d9
Emerson Herzeg <emer.herzeg@gmail.com>
2024-02-07 15:36:32 -0300

    fix: add NesoiStrings

--- src/strings.ts
+++ src/strings.ts
@@ -6,7 +6,8 @@ export const NesoiStrings = {
     'task.advance.log': 'Advanced',
     'task.execute.log': 'Executed',
     'task.graph.child_of': 'Linked as child of task',
-    'task.graph.parent_of': 'Linked as parent of task'
+    'task.graph.parent_of': 'Linked as parent of task',
+    'task.graph.retates_to': 'Linked as relates to task'
 
 }
 
